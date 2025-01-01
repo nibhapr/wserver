44c108a19fe318ea895e56312c9d220c9b77762f
IamShaDoW666 <www.gaming2018@gmail.com>
2025-01-01 22:43:13 +0530

    update notifications shopify:

--- dist/routes/webhook.js
+++ dist/routes/webhook.js
@@ -1,9 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
+const __1 = require("..");
 const router = (0, express_1.Router)();
-router.post("/order-payment", (req, res) => {
-    console.log(req.body);
+router.post("/order-payment", async (req, res) => {
+    var _a;
+    console.log(req);
+    const client = __1.sessions.get("917012749946");
+    const result = await (client === null || client === void 0 ? void 0 : client.onWhatsApp("917902708908"));
+    await (client === null || client === void 0 ? void 0 : client.sendMessage(result ? result[0].jid : "", {
+        text: (_a = JSON.stringify(req)) !== null && _a !== void 0 ? _a : "",
+    }));
     res.status(200).json({ message: "sent!", status: true });
 });
 router.post("/cart-create", (req, res) => {
--- src/routes/webhook.ts
+++ src/routes/webhook.ts
@@ -1,8 +1,14 @@
 import { Router } from "express";
+import { sessions } from "..";
 const router = Router();
 
-router.post("/order-payment", (req, res) => {
-  console.log(req.body);
+router.post("/order-payment", async (req, res) => {
+  console.log(req);
+  const client = sessions.get("917012749946");
+  const result = await client?.onWhatsApp("917902708908");
+  await client?.sendMessage(result ? result[0].jid : "", {
+    text: JSON.stringify(req) ?? "",
+  });
   res.status(200).json({ message: "sent!", status: true });
 });
 
