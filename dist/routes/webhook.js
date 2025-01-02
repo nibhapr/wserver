971fa3cac2f48fc80c733a2a2816e4e44adcf85c
IamShaDoW666 <www.gaming2018@gmail.com>
2025-01-02 18:36:46 +0530

    new build

--- dist/routes/webhook.js
+++ dist/routes/webhook.js
@@ -14,7 +14,9 @@ router.post("/order-payment", async (req, res) => {
     const client = __1.sessions.get("917012749946");
     const result = await (client === null || client === void 0 ? void 0 : client.onWhatsApp(req.body["billing_address"]["phone"].replace(/\D/g, "")));
     await (client === null || client === void 0 ? void 0 : client.sendMessage(result ? result[0].jid : "", {
-        text: "Your order has been received!",
+        text: "Your order has been confirmed tracking number [Tracking Number]. Delivery within two working days. For any inquiries, WhatsApp us at +971563680897. Watch product videos on our social media: https://linktr.ee/chenarabia?utm_source=linktree_profile_share&ltsid=a25f405b-ead5-4d39-9502-b5b102f1b6a4",
+        image: { url: "https://lovosis.in/logo.jpg" },
+        caption: "your order"
     }));
     res.status(200).json({ message: "sent!", status: true });
 });
@@ -23,7 +25,9 @@ router.post("/cart-create", async (req, res) => {
     const client = __1.sessions.get("917012749946");
     const result = await (client === null || client === void 0 ? void 0 : client.onWhatsApp(req.body["billing_address"]["phone"].replace(/\D/g, "")));
     await (client === null || client === void 0 ? void 0 : client.sendMessage(result ? result[0].jid : "", {
-        text: "Your order has been received!",
+        text: "Your order has been confirmed tracking number [Tracking Number]. Delivery within two working days. For any inquiries, WhatsApp us at +971563680897. Watch product videos on our social media: https://linktr.ee/chenarabia?utm_source=linktree_profile_share&ltsid=a25f405b-ead5-4d39-9502-b5b102f1b6a4",
+        image: { url: "https://lovosis.in/logo.jpg" },
+        caption: "your order"
     }));
     res.status(200).json({ message: "sent!", status: true });
 });
