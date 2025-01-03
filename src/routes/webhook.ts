6855ab69fd2098f5459fb104c6cf377ed70656b6
IamShaDoW666 <www.gaming2018@gmail.com>
2025-01-03 10:40:41 +0530

    new test

--- src/routes/webhook.ts
+++ src/routes/webhook.ts
@@ -26,7 +26,7 @@ router.post(
 
 router.post("/order-create", async (req, res) => {
   console.log(req.body["billing_address"]["phone"]);
-  const client = sessions.get("971581439355"); // 971581439355
+  const client = sessions.get("917012749946"); // 971581439355
   const result = await client?.onWhatsApp(
     req.body["billing_address"]["phone"].replace(/\D/g, "")
   );
@@ -41,6 +41,21 @@ router.post("/order-create", async (req, res) => {
 });
 
 router.get("/customer-create", async (req, res) => {
+  console.log(req.body);
+  const client = sessions.get("917012749946");
+  const result = await client?.onWhatsApp(
+    req.body["billing_address"]["phone"].replace(/\D/g, "")
+  );
+  await client?.sendMessage(result ? result[0].jid : "", {
+    image: { url: "https://lovosis.in/logo.jpg" },
+    mimetype: "image/jpeg",
+    caption: `Your order has been confirmed tracking number ${
+      req.body["number"] ?? "[Tracking Number]"
+    }. Delivery within two working days. For any inquiries, WhatsApp us at +971563680897. Watch product videos on our social media: https://linktr.ee/chenarabia?utm_source=linktree_profile_share&ltsid=a25f405b-ead5-4d39-9502-b5b102f1b6a4`,
+  });
+  res.status(200).json({ message: "Hello World!" });
+});
+router.get("/test", async (req, res) => {
   console.log(req.body);
   res.status(200).json({ message: "Hello World!" });
 });
