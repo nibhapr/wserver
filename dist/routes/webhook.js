a6345453ad523693dcbc9c02fb5a37323fd33240
IamShaDoW666 <www.gaming2018@gmail.com>
2025-01-03 14:28:22 +0530

    new build

--- dist/routes/webhook.js
+++ dist/routes/webhook.js
@@ -25,7 +25,7 @@ router.post("/order-create", async (req, res) => {
     const client = __1.sessions.get("917012749946"); // 971581439355
     const result = await (client === null || client === void 0 ? void 0 : client.onWhatsApp(req.body["billing_address"]["phone"].replace(/\D/g, "")));
     await (client === null || client === void 0 ? void 0 : client.sendMessage(result ? result[0].jid : "", {
-        text: `Hi ${req.body["billing_address"]["first_name"]}.Thank you for your order! 🎉\n\n Here are your order details:\n\n order number:${req.body["order_number"]}\n\n Track your order: ${req.body["order_status_url"]}\n\nFor any inquiries, WhatsApp us at +971563680897.\n\n`,
+        text: `Hi ${req.body["billing_address"]["first_name"]}.Thank you for your order! 🎉\n\n Here are your order details:\n order number:${req.body["order_number"]}\n Track your order: ${req.body["order_status_url"]}\n\nFor any inquiries, WhatsApp us at +971563680897.\nThank you for shopping with us 😊:\n *https://chenarabia.com*`,
     }));
     res.status(200).json({ message: "sent!", status: true });
 });
