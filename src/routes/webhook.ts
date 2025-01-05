0124b083498117d7a921cbd4f43ef588e0130973
IamShaDoW666 <www.gaming2018@gmail.com>
2025-01-05 23:34:03 +0530

    new

--- src/routes/webhook.ts
+++ src/routes/webhook.ts
@@ -82,7 +82,7 @@ router.post("/fullfilment_creation", async (req, res) => {
 });
 
 router.post("/test", async (req, res) => {
-  console.log(req.body);
+  console.log(req.body["first_name"]);
   res.status(200).json({ message: "sent!", status: true });
 });
 export default router;
