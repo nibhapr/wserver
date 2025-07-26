4ae3f44c87fb106320c95c149b9140fc860e61fd
IamShaDoW666 <www.gaming2018@gmail.com>
2025-07-27 02:28:03 +0530

    Harcode redis

--- dist/utils/redis.js
+++ dist/utils/redis.js
@@ -6,11 +6,19 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.blastQueue = exports.redis = void 0;
 const bullmq_1 = require("bullmq");
 const ioredis_1 = __importDefault(require("ioredis"));
+// export const redis = new Redis({
+//   port: Number(process.env.REDIS_PORT) || 6379, // Defaults to 6379
+//   host: process.env.REDIS_HOST,
+//   username: process.env.REDIS_USERNAME,
+//   password: process.env.REDIS_PASSWORD,
+//   db: 0, // Defaults to 0
+//   maxRetriesPerRequest: null
+// })
 exports.redis = new ioredis_1.default({
-    port: Number(process.env.REDIS_PORT) || 6379, // Defaults to 6379
-    host: process.env.REDIS_HOST,
-    username: process.env.REDIS_USERNAME,
-    password: process.env.REDIS_PASSWORD,
+    port: 19327,
+    host: "redis-19327.c212.ap-south-1-1.ec2.redns.redis-cloud.com",
+    username: "default",
+    password: "O3dtw8l8mkxs7iJGz2JQLxwHainPsQch",
     db: 0, // Defaults to 0
     maxRetriesPerRequest: null
 });
