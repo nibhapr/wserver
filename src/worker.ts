566b805483b6265d81c7340b41dfcf9106815e42
IamShaDoW666 <www.gaming2018@gmail.com>
2025-07-29 00:31:32 +0530

    add initialisation for worker

--- src/worker.ts
+++ src/worker.ts
@@ -1,12 +1,13 @@
-import { Worker } from 'bullmq';
+import { Queue, Worker } from 'bullmq';
 import { redis } from './utils/redis';
 import NodeCache from 'node-cache';
 import { startWhatsAppSession } from './lib/whatsapp';
 import logger from './utils/logger';
+import prisma from './utils/db';
+import { sleep } from './utils/common';
 
 export const sessions = new Map();
 export const msgRetryCounterCache = new NodeCache();
-
 
 new Worker('whatsapp-jobs', async (job) => {
   logger.info(`Processing job: ${job.name} for session: ${job.data.number}`);
@@ -23,6 +24,8 @@ new Worker('whatsapp-jobs', async (job) => {
         try {
           console.log(`Sending message to ${to} from session ${number}`);
           //TODO: Good place to add delay
+          const randomDelay = Math.floor(Math.random() * 1000) + 500; // Random delay between 500ms and 1500ms
+          await sleep(randomDelay);
           await sock.sendMessage(`${to}@s.whatsapp.net`, { text });
         } catch (error) {
           console.error('Failed to send message:', error);
@@ -36,3 +39,20 @@ new Worker('whatsapp-jobs', async (job) => {
       break;
   }
 }, { connection: redis });
+
+async function initializeWorker() {
+  logger.info('WhatsApp worker initialized');
+  const queue = new Queue('whatsapp-jobs', {
+    connection: redis,
+  })
+  const numbers = await prisma.numbers.findMany();
+  numbers.forEach(number => {
+    queue.add('connect-whatsapp', { number: number.body }, {
+      delay: 1000, // Delay to avoid overwhelming the WhatsApp API
+      attempts: 1,
+    });
+  })
+}
+
+
+initializeWorker()
